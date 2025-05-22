32ec8e41590093d69f4587708fa5437f0eee8031
mpoligang <michelangelopoli21@gmail.com>
2025-05-22 21:07:44 +0200

    fixed mainTheme

--- src/app/shared/consts/MainTheme.ts
+++ src/app/shared/consts/MainTheme.ts
@@ -1,6 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
+
 import Aura from "@primeng/themes/aura";
 import { definePreset } from "@primeng/themes";
 
