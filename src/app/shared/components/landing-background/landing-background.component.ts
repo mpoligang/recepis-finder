84867d3a58f7d065ca3f34453517d9372fe35b35
mpoligang <michelangelopoli21@gmail.com>
2025-05-25 18:13:52 +0200

    added imageLoaderservice to landing-background component

--- src/app/shared/components/landing-background/landing-background.component.ts
+++ src/app/shared/components/landing-background/landing-background.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ImageLoaderService } from './../../services/image-loader/image-loader.service';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { SkeletonModule } from 'primeng/skeleton';
 
 @Component({
@@ -7,4 +8,16 @@ import { SkeletonModule } from 'primeng/skeleton';
   templateUrl: './landing-background.component.html',
   styleUrl: './landing-background.component.scss'
 })
-export class LandingBackgroundComponent { }
+export class LandingBackgroundComponent implements OnInit {
+
+  public image = signal<string>('/food_background.jpg');
+  public imageLoaded = signal<boolean>(false);
+
+
+  private readonly imageLoaderService = inject(ImageLoaderService);
+
+  ngOnInit(): void {
+    this.imageLoaderService.handleLoadingImage(this.image(), this.imageLoaded);
+  }
+
+}
